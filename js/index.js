27a6d17d32d6a31807dcea7a285333ec6db48539
anomis08 <simona.marchese@student.howest.be>
2022-10-20 11:16:37 +0200

    Added confetti

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
+import JSConfetti from '../node_modules/js-confetti/dist/es/index.js';
 
 const init = () => {
-  const $form = document.querySelector("form");
-  $form.addEventListener("submit", (event) => {
-    event.preventDefault();
-  });
+    const $form = document.querySelector("form");
+    const jsConfetti = new JSConfetti();
+    $form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        jsConfetti.addConfetti();
+    });
 }
 
 
